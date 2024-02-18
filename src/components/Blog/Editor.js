87b20533f6c33196cb0cf8128fee0cab6fc0bc9f
fragilebanana16 <850604714@qq.com>
2024-02-18 21:38:58 +0800

    博客-富文本编辑器ReactQuill自定义滚动条

--- src/components/Blog/Editor.js
+++ src/components/Blog/Editor.js
@@ -1,6 +1,10 @@
 import ReactQuill from "react-quill";
+import styled from "styled-components";
+import { useTheme } from "@mui/material/styles";
 
 export default function Editor({value,onChange}) {
+  const theme = useTheme();
+
   const modules = {
     toolbar: [
       [{ header: [1, 2, false] }],
@@ -16,12 +20,54 @@ export default function Editor({value,onChange}) {
     ],
   };
   return (
+    <Container>
     <div className="content">
     <ReactQuill
+    style={{height:'530px'}}
       value={value}
       theme={'snow'}
       onChange={onChange}
-      modules={modules} />
+      modules={modules} 
+      />
     </div>
+    </Container>
+
   );
 }
+
+const Container = styled.div`
+.ql-editor{
+  ${'' /* overflow: "hidden";
+  overflow-y:'auto'; */}
+  ${'' /* scrollbar-color:#888 #00f5f5; */}
+  ${'' /* scrollbar-width: thin;
+  height: "100%";
+            position: "relative";
+            flexGrow: 1;
+            overflow: "hidden";
+            overflowY: "scroll"; */}
+}
+
+.ql-editor::-webkit-scrollbar{
+  width: 10px;;
+}
+
+.ql-editor::-webkit-scrollbar-track {
+  background-color: #f1f1f1;
+  border-radius: 10px;
+}
+
+.ql-editor::-webkit-scrollbar-thumb{
+  background-color: #8885;
+  border-radius: 20px;
+}
+
+.ql-editor::-webkit-scrollbar-thumb:hover{
+  background-color: #888;
+}
+
+.content{
+  ${'' /* background-color: coral; */}
+  ${'' /* toolbar height 42px*/}
+  padding-bottom: calc(42px + 10px); 
+`;
--- src/pages/dashboard/BlogCreatePost.js
+++ src/pages/dashboard/BlogCreatePost.js
@@ -1,14 +1,14 @@
 import ReactQuill from "react-quill";
 import 'react-quill/dist/quill.snow.css';
-import {useState} from "react";
-import {Navigate} from "react-router-dom";
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
 import styled from "styled-components";
 import Editor from "../../components/Blog/Editor";
-
+import { TextField, Button, FormControl, FormControlLabel, } from "@mui/material";
 export default function BlogCreatePost() {
-  const [title,setTitle] = useState('');
-  const [summary,setSummary] = useState('');
-  const [content,setContent] = useState('');
+  const [title, setTitle] = useState('');
+  const [summary, setSummary] = useState('');
+  const [content, setContent] = useState('');
   const [files, setFiles] = useState('');
   const [redirect, setRedirect] = useState(false);
   async function createNewPost(ev) {
@@ -33,24 +33,56 @@ export default function BlogCreatePost() {
   }
   return (
     <Container>
-    <form onSubmit={createNewPost}>
-      <input type="title"
-             placeholder={'Title'}
-             value={title}
-             onChange={ev => setTitle(ev.target.value)} />
-      <input type="summary"
-             placeholder={'Summary'}
-             value={summary}
-             onChange={ev => setSummary(ev.target.value)} />
-      <input type="file"
-             onChange={ev => setFiles(ev.target.files)} />
-      <Editor value={content} onChange={setContent} />
-      <button style={{marginTop:'5px'}}>Create post</button>
-    </form>
+      <form>
+        <TextField
+          fullWidth
+          style={{ margin: "5px 0px" }}
+          type="text"
+          label="Title"
+          variant="outlined"
+        />
+        <br />
+        <TextField
+          fullWidth
+          style={{ margin: "5px 0px" }}
+          type="text"
+          label="Summary"
+          variant="outlined"
+        />
+        <br />
+        <Editor value={content} onChange={setContent} />
+        <Button
+          variant="contained"
+          component="label"
+        >
+          Upload File
+          <input
+            type="file"
+            hidden
+          />
+        </Button>
+        <Button style={{ width: "30%", margin: "0px 20px" }} variant="contained" color="primary">
+          Create post
+        </Button>
+      </form>
+
+      {/* <form onSubmit={createNewPost}>
+        <input type="title"
+          placeholder={'Title'}
+          value={title}
+          onChange={ev => setTitle(ev.target.value)} />
+        <input type="summary"
+          placeholder={'Summary'}
+          value={summary}
+          onChange={ev => setSummary(ev.target.value)} />
+        <input type="file"
+          onChange={ev => setFiles(ev.target.files)} />
+        <button style={{ marginTop: '5px' }}>Create post</button>
+      </form> */}
     </Container>
   );
 }
 
 const Container = styled.div`
- 
+
 `;
